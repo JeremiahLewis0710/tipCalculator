c495ce50ff7cc264b0d9b98b762bd95abcaaf7e1
JeremiahLewis0710 <jeremiahlewis@JeremiahLewisMBP.local>
2016-09-27 16:35:24 -0400

    Calculator doing proper calculations. Some styling added

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,20 +1,34 @@
 $(function () {
-  $("#Calculate").click(function () {
-    var bill = $("#Bill").val();
-    var percentage = $("#Quality").find(":selected").val();
-    var people = $("#Split").val();
-    var total = bill * percentage / people
-    console.log(total.toFixed(2));
-    var div = $("<div></div>");
-    div.append(total.toFixed(2))
-    $(".returntip").append(div);
-    return false;
+
 
   });
 
 
+$("#Calculate").click(function () {
+    var bill = $("#Bill").val();
+    var percentage = $("#Quality").find(":selected").val();
+    var people = $("#Split").val();
+    var tip = (Math.round((bill * percentage / people)*100)/100).toFixed(2);
+    var total = (parseFloat(bill) + parseFloat(tip)).toFixed(2);
+    console.log(total);
+
+    var div = $("<div></div>");
+    div.append("<h3> Your total bill with tip is: "+ total+"</h3>")
+       .append("<h3>Your tip comes to:" +tip +"</h3>")
+    $(".returntip").append(div);
+
+    if(bill==0){
+      alert("Error Please Enter Valid Number")
+      return null;
+    }
+    else{
+    return false;
+    };
 
 });
+
+
+
 
 
 
